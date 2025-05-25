9b6937ac534ab69dc0c6664f4b1be9dcd860915e
Saymon Rodrigues <saymonlee25@hotmail.com>
2025-05-25 16:46:14 -0300

    WIP: topics by channel

---
+++ database/factories/ChannelFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Channel;
+use App\Model;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+$factory->define(Channel::class, function (Faker $faker) {
+        $name = $faker->sentence;
+    return [
+        'name' => $name,
+        'description' => $faker->sentence,
+        'slug' => Str::slug($name)
+    ];
+});
--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -2,14 +2,24 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Channel;
 use App\Model;
 use App\Thread;
+use App\User;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 $factory->define(Thread::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        'title' => $faker->sentence,
+        'channel_id' => function() {
+            return factory(Channel::class)->create()->id;
+        },
+        'user_id' => function() {
+    return factory(User::class)->create()->id;
+        },
+        'title' => $title,
         'body' => $faker->paragraph(2),
-        'slug' => $faker->slug
+        'slug' => Str::slug($title)
     ];
 });
