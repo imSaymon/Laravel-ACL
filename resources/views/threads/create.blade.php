fe41711a848c279fd5b71c4c461eb8bed63c7b6c
Saymon Rodrigues <saymonlee25@hotmail.com>
2025-05-25 22:31:46 -0300

    WIP: channel-menu

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -9,6 +9,14 @@
     <div class="col-12">
         <form action="{{route('threads.store')}}" method="post">
             @csrf
+            <div class="form-group">
+                <label for="">Escolha Um Canal Para O Tópico</label>
+                <select name="channel_id" id="" class="form-control">
+                    @foreach($channels as $channel)
+                        <option value="{{$channel->id}}">{{$channel->name}}</option>
+                    @endforeach
+                </select>
+            </div>
 
             <div class="form-group">
                 <label for="">Titulo Tópico</label>
